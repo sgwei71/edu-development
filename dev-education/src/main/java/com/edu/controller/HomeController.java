f505beb9269d2e0004047fe1c598214f3ce06004
sgwei71 <34912335+sgwei71@users.noreply.github.com>
2022-12-09 23:55:19 +0900

    Update HomeController.java

--- dev-education/src/main/java/com/edu/controller/HomeController.java
+++ dev-education/src/main/java/com/edu/controller/HomeController.java
@@ -5,13 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @Controller
-//@RequestMapping("api/v1/")
-//@RestController
 public class HomeController {
-	@GetMapping("/home1")
+	@GetMapping("/health")
 	public String home(Model model) {
-		model.addAttribute("title","정상페이지 ");
+		model.addAttribute("title","정상페이지");
 		return "home";
 	}
 }

7e7b7697d3716dff4ad9e7f0f21a0e48aaea2c19
sgwei71 <34912335+sgwei71@users.noreply.github.com>
2022-12-09 23:46:15 +0900

    Update HomeController.java

--- dev-education/src/main/java/com/edu/controller/HomeController.java
+++ dev-education/src/main/java/com/edu/controller/HomeController.java
@@ -3,7 +3,8 @@ package com.edu.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 //@Controller
 @RequestMapping("api/v1/")
 @RestController

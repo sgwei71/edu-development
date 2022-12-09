77389b28ed0cda2942a3ce92549e635819c48053
sgwei71 <34912335+sgwei71@users.noreply.github.com>
2022-12-09 23:49:55 +0900

    Update HomeController.java

--- dev-education/src/main/java/com/edu/controller/HomeController.java
+++ dev-education/src/main/java/com/edu/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 //@Controller
-@RequestMapping("api/v1/")
+//@RequestMapping("api/v1/")
 @RestController
 public class HomeController {
 	@GetMapping("/home1")

312c85ad7bb11d7903c2ec9af3657edd65c468ce
sgwei71 <34912335+sgwei71@users.noreply.github.com>
2022-12-09 23:18:07 +0900

    Update HomeController.java
    변경하자

--- dev-education/src/main/java/com/edu/controller/HomeController.java
+++ dev-education/src/main/java/com/edu/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 	@GetMapping("/home1")
 	public String home(Model model) {
-//		model.addAttribute("title","사용자가입");
+		model.addAttribute("title","사용자가입");
 		return "home";
 	}
 }
